e2350e0a1e317fc603a95e29dc74028a082b1e67
mjoze <mirek.jozefiak@gmail.com>
2020-01-30 14:53:43 +0100

    add templates and static files

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'MJ',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 01, 2020'
+    },
+    {
+        'author': 'MJ',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'January 30, 2020'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
