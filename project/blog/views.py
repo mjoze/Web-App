a31ddb976f654c3b85d34b54f89a2c76126fc293
mjoze <mirek.jozefiak@gmail.com>
2020-02-02 18:51:16 +0100

    database_migrations

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'MJ',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 01, 2020'
-    },
-    {
-        'author': 'MJ',
-        'title': 'Blog Post 2',
-        'content': 'second post content',
-        'date_posted': 'January 30, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
